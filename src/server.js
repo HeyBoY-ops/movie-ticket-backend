da22abfcefa6058bab769b27112e5e26b68edcc6
HeyBoY-ops <abhi9835003292@gmail.com>
2025-11-22 17:33:37 +0530

    Fix CORS for Vercel domain

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://movie-ticket-app-drab.vercel.app"
+      "https://movie-ticket-gfcrrajhv-abhisheks-projects-a1a026b5.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
