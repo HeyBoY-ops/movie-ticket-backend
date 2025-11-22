4ba8f9efa735d1c48f9392a75dc2584eee0a6e85
HeyBoY-ops <abhi9835003292@gmail.com>
2025-11-22 19:03:33 +0530

    .

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://movie-ticket-app-drab.vercel.app/login"
+      "https://movie-ticket-app-drab.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
